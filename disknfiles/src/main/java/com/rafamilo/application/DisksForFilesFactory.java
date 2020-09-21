20749a3ec9e642059b5d01b4c27940125a844b72
rafael.melges <rafael.melges@azi.com.br>
2020-09-21 10:53:02 -0400

    do a little refactor

--- disknfiles/src/main/java/com/rafamilo/application/DisksForFilesFactory.java
+++ disknfiles/src/main/java/com/rafamilo/application/DisksForFilesFactory.java
@@ -4,7 +4,6 @@ import com.rafamilo.usecases.DiskForFiles;
 import com.rafamilo.usecases.IDiskForFiles;
 
 public abstract class DisksForFilesFactory {
-
     public static IDiskForFiles diskForFiles() {
         return new DiskForFiles();
     }
